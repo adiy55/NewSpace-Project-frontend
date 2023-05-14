3fa652873ad77191f69aeda3afee4bb8b25abb56
adiy55 <adiyafe55@gmail.com>
2023-05-14 21:28:02 +0300

    Add ImagePicker package

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, Button, Image } from "react-native";
 import { screenStyles } from "../../styles";
+import * as ImagePicker from "expo-image-picker";
 
 const HomeScreen = () => {
+  const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+      });
+
+      console.log(result.uri);
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={screenStyles.container}>
       <Text>Home Screen!</Text>
       <StatusBar style="auto" />
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 };
