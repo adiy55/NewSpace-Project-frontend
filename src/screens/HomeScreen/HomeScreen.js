4c256151afe8bc249969188fe172cc0af9aa5265
adiy55 <adiyafe55@gmail.com>
2023-05-16 21:08:19 +0300

    Update expo version + update HomeScreen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Button, Image } from "react-native";
+import { Text, ScrollView, Image, SafeAreaView } from "react-native";
 import { screenStyles } from "../../styles";
 import * as ImagePicker from "expo-image-picker";
+import { Button } from "react-native-paper";
 
 const HomeScreen = () => {
   const [image, setImage] = useState(null);
   const pickImage = async () => {
     try {
+      // exif = true to get metadata of image
       let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
+        allowsEditing: false,
+        exif: true,
       });
-
-      console.log(result.uri);
       if (!result.cancelled) {
         setImage(result.uri);
+        console.log(result.exif);
       }
     } catch (error) {
       console.error(error);
@@ -23,14 +25,26 @@ const HomeScreen = () => {
   };
 
   return (
-    <View style={screenStyles.container}>
-      <Text>Home Screen!</Text>
-      <StatusBar style="auto" />
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
-    </View>
+    <ScrollView contentContainerStyle={screenStyles.container}>
+      <SafeAreaView>
+        <Text>Home Screen!</Text>
+        <StatusBar style="auto" />
+        <Button mode="contained" onPress={pickImage}>
+          Pick an image from camera roll
+        </Button>
+        {image && (
+          <>
+            <Image
+              source={{ uri: image }}
+              style={{ margin: 20, width: 200, height: 200 }}
+            />
+            <Button mode="contained" onPress={() => setImage(null)}>
+              Clear Selection
+            </Button>
+          </>
+        )}
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
