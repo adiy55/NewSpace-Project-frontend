b7d2a95443b74e842ea33a590a02a6404d1b37cf
adiy55 <adiyafe55@gmail.com>
2023-05-17 16:33:14 +0300

    Update home screen image selection + add useFocusEffect

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "./src/navigation/Navigator";
 
 export default function App() {
   return (
     <PaperProvider>
-      <Navigator />
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
     </PaperProvider>
   );
 }
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -4,47 +4,59 @@ import { Text, ScrollView, Image, SafeAreaView } from "react-native";
 import { screenStyles } from "../../styles";
 import * as ImagePicker from "expo-image-picker";
 import { Button } from "react-native-paper";
+import { useFocusEffect } from "@react-navigation/native";
 
 const HomeScreen = () => {
-  const [image, setImage] = useState(null);
+  const [images, setImages] = useState([]);
+
   const pickImage = async () => {
     try {
       // exif = true to get metadata of image
-      let result = await ImagePicker.launchImageLibraryAsync({
+      const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: false,
+        allowsMultipleSelection: true,
+        selectionLimit: 2,
+        base64: true,
         exif: true,
       });
       if (!result.cancelled) {
-        setImage(result.uri);
-        console.log(result.exif);
+        const imageArray = result.selected.filter((img) => !img.cancelled);
+        setImages(imageArray);
+        console.log("ARR", imageArray);
       }
+      setLoading(false);
     } catch (error) {
       console.error(error);
+      setImages([]);
     }
   };
 
+  useFocusEffect(React.useCallback(() => {}, [images]));
+
   return (
-    <ScrollView contentContainerStyle={screenStyles.container}>
-      <SafeAreaView>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={screenStyles.container}>
         <Text>Home Screen!</Text>
         <StatusBar style="auto" />
         <Button mode="contained" onPress={pickImage}>
           Pick an image from camera roll
         </Button>
-        {image && (
+        {images.length > 0 && (
           <>
-            <Image
-              source={{ uri: image }}
-              style={{ margin: 20, width: 200, height: 200 }}
-            />
-            <Button mode="contained" onPress={() => setImage(null)}>
+            {images.map((img, idx) => (
+              <Image
+                key={idx}
+                source={img.uri}
+                style={{ margin: 20, width: 200, height: 200 }}
+              />
+            ))}
+            <Button mode="contained" onPress={() => setImages([])}>
               Clear Selection
             </Button>
           </>
         )}
-      </SafeAreaView>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
